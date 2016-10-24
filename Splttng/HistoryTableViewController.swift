f2b9be088df4ad525737cfa28aa7b836aec7301f
Emiliano Hualpa <ehualpa@gmail.com>
2016-10-24 22:14:51 +0200

    Updated History and deleted files

--- Splttng/HistoryTableViewController.swift
+++ Splttng/HistoryTableViewController.swift
@@ -9,43 +9,46 @@
 import UIKit
 
 class HistoryTableViewController: UITableViewController {
-
+    
+    
+    struct HistoryObjects {
+        var sectionName : String?
+        var sectionObjects : [String]?
+    }
+    
+    var historyArray : [HistoryObjects]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        historyArray = [
+            HistoryObjects(sectionName: "Section 1", sectionObjects: ["obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3"]),
+            HistoryObjects(sectionName: "Section 2", sectionObjects: ["obj3","obj4","obj5","obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3"])]
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return historyArray?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return historyArray?[section].sectionObjects?.count ?? 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historyCell", for: indexPath)
 
-        // Configure the cell...
-
+        let history = historyArray?[indexPath.section].sectionObjects?[indexPath.row]
+        
+        cell.textLabel?.text = history
+        
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return historyArray?[section].sectionName ?? "No name"
+    }
 
     /*
     // Override to support conditional editing of the table view.
