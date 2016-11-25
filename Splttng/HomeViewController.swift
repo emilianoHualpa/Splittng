5b31418a4183282f2bcc038b9f991ac5bf3700a7
Emiliano Hualpa <ehualpa@gmail.com>
2016-11-25 12:41:04 +0100

    UI changes on the Home page

--- Splttng/HomeViewController.swift
+++ Splttng/HomeViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let buttonsCornerRadius : CGFloat = 5.0
     
     @IBOutlet weak var logoView: UIImageView!
     @IBOutlet weak var splitCheckButton: UIButton!
     @IBOutlet weak var divideMealsButton: UIButton!
     @IBOutlet weak var historyButton: UIButton!
+    @IBOutlet weak var buttonContainerView: UIView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupButtonsText()
+        roundContainerView()
 
+    }
+    
+    func roundContainerView()
+    {
+        buttonContainerView.layer.cornerRadius = buttonsCornerRadius
+        buttonContainerView.clipsToBounds = true
     }
     
     //Let's dinamically set up the labels on the buttons.
