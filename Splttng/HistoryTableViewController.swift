c4e4cd93f8335d11d71c65085977b055b0300416
Emiliano Hualpa <ehualpa@gmail.com>
2016-11-25 12:56:31 +0100

    Refactor HistoryViewController adding extensions for adopting table view protocols

--- Splttng/HistoryTableViewController.swift
+++ Splttng/HistoryTableViewController.swift
@@ -13,87 +13,55 @@ struct HistoryObjects {
     var sectionObjects : [String]?
 }
 
-class HistoryTableViewController: UITableViewController {
+class HistoryTableViewController: UIViewController {
     
     var historyArray : [HistoryObjects]?
+    @IBOutlet weak var tableView: UITableView!
+    
+    let historyCellIdentifier = "historyCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self;
+        self.tableView.dataSource = self;
 
         //TODO: Hardcoded data
         
         historyArray = [
-            HistoryObjects(sectionName: "Section 1", sectionObjects: ["obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3"]),
-            HistoryObjects(sectionName: "Section 2", sectionObjects: ["obj3","obj4","obj5","obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3"])]
+            HistoryObjects(sectionName: "Checks", sectionObjects: ["obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3"]),
+            HistoryObjects(sectionName: "Group Expenses", sectionObjects: ["obj3","obj4","obj5","obj1","obj2","obj3","obj1","obj2","obj3","obj1","obj2","obj3"])]
     }
+}
 
-    // MARK: - Table view data source
+// MARK: - Table view delegate
+extension HistoryTableViewController : UITableViewDelegate {
+    
+    
+}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+// MARK: - Table view data source
+extension HistoryTableViewController : UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return historyArray?.count ?? 0
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return historyArray?[section].sectionObjects?.count ?? 0
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "historyCell", for: indexPath)
-
-        let history = historyArray?[indexPath.section].sectionObjects?[indexPath.row]
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: historyCellIdentifier, for: indexPath)
         
+        let history = historyArray?[indexPath.section].sectionObjects?[indexPath.row]
         cell.textLabel?.text = history
         
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return historyArray?[section].sectionName ?? "No name"
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
